a82f38a9cfe334143668a24eaecf2eb2ad367ce2
hecklinderjatin <itsjstar07@gmail.com>
2024-02-19 22:42:17 +0530

    sample added

--- styles/src/gui/frame.py
+++ styles/src/gui/frame.py
@@ -1,31 +1,31 @@
 import tkinter
 import tkinter.ttk as ttk
 import customtkinter
-
-customtkinter.set_appearance_mode("dark")
-
-app = customtkinter.CTk()
-app.geometry("1000x500")
-app.title("STYLES")
+from logic.functions import *
 
 
-app.rowconfigure(0, weight=1)  
-app.columnconfigure(0, weight=1)  
-app.columnconfigure(1, weight=4)  
-app.columnconfigure(2, weight=1)  
+def run_tkinter_app():
+    customtkinter.set_appearance_mode("dark")
 
+    app = customtkinter.CTk()
+    app.geometry("1000x500")
+    app.title("STYLES")
 
-frame_lay = customtkinter.CTkScrollableFrame(app, orientation="vertical", label_text="Layout", corner_radius=0)
-frame_lay.grid(row=0, column=0, rowspan=2, pady=10, padx=10, sticky="nsew")
+    app.rowconfigure(0, weight=1)  
+    app.columnconfigure(0, weight=1)  
+    app.columnconfigure(1, weight=4)  
+    app.columnconfigure(2, weight=1)  
 
-frame_canv = tkinter.Frame(master=app)
-frame_canv.grid(padx=20, pady=20, row=0, column=1, rowspan=3, sticky="nsew")
+    frame_lay = customtkinter.CTkScrollableFrame(app, orientation="vertical", label_text="Layout", corner_radius=0)
+    frame_lay.grid(row=0, column=0, rowspan=2, pady=10, padx=10, sticky="nsew")
 
-frame_inps = customtkinter.CTkScrollableFrame(app, orientation="vertical", label_text="Inspector", corner_radius=0)
-frame_inps.grid(row=0, column=2, rowspan=2, pady=10, padx=10, sticky="nsew")
+    frame_canv = tkinter.Frame(master=app)
+    frame_canv.grid(padx=20, pady=20, row=0, column=1, rowspan=3, sticky="nsew")
 
+    frame_inps = customtkinter.CTkScrollableFrame(app, orientation="vertical", label_text="Inspector", corner_radius=0)
+    frame_inps.grid(row=0, column=2, rowspan=2, pady=10, padx=10, sticky="nsew")
 
-ttk_style = ttk.Style()
-ttk_style.configure(frame_lay.winfo_class(), background='red')
+    ttk_style = ttk.Style()
+    ttk_style.configure(frame_lay.winfo_class(), background='red')
 
-app.mainloop()
+    app.mainloop()
